5b56d3d5210faf58e0fb4af2e54bc91f5bd13f42
AlyAbdulloh <uwongsabar12@gmail.com>
2023-11-28 18:55:26 +0700

    add: layouts

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'Dashboard')
+
+@section('page_name', 'Dashboard')
+
+@section('content')
+
+@endsection
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,24 @@
+<ul class="sidebar-menu">
+    <li class="menu-header">Dashboard</li>
+    <li class="{{ Request::is('/') ? 'active' : '' }}"><a class="nav-link" href="/dashboard"><i class="fas fa-fire"></i>
+            <span>Dashboard</span></a></li>
+    <li class="menu-header">Data</li>
+    {{-- <li class="{{ Request::is('admin') ? 'active' : '' }}"><a class="nav-link" href="/admin"><i class="fas fa-user"></i>
+            <span>Admin</span></a></li> --}}
+    <li class="nav-item dropdown {{ Request::is('books') || Request::is('transactions') ? 'active' : '' }}">
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i>
+            <span>Alternatif</span></a>
+        <ul class="dropdown-menu">
+            <li class="{{ Request::is('books') ? 'active' : '' }}"><a class="nav-link" href="/books">
+                    Alternatif</a></li>
+            <li class="{{ Request::is('transactions') ? 'active' : '' }}"><a class="nav-link" href="/transactions">
+                    Nilai Alternatif</a></li>
+        </ul>
+    </li>
+    <li class="{{ Request::is('users') ? 'active' : '' }}"><a class="nav-link" href="/users"><i
+                class="fas fa-th-large"></i>
+            <span>Kriteria</span></a></li>
+    <li class="{{ Request::is('users') ? 'active' : '' }}"><a class="nav-link" href="/users"><i
+                class="fas fa-calculator"></i>
+            <span>Perhitungan</span></a></li>
+</ul>
